3fd1d1f68a1f7dd777393c7fbf9a2154eda6d1cc
Brian C <bryancart6@gmail.com>
2025-02-04 02:15:45 +0000

    done

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,25 @@
---create inpFunc
-inpFunc = [1..5] 
-
---Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+-- Define applicatorFunc
+applicatorFunc inpFunc s =
+  if s == 's' 
+    then fromIntegral (sum inpFunc)  -- sum of list as a float
+    else fromIntegral (sum inpFunc) / fromIntegral (length inpFunc)  -- average
 
 main = do
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
-    putStrLn("sum = " ++ show(result))
+    -- Ask for the start and end values of the range
+    putStrLn "Enter the start value of the range (a):"
+    a <- readLn
+    putStrLn "Enter the end value of the range (b):"
+    b <- readLn
+    
+    -- Generate the list based on the range [a..b]
+    let inpFunc = [a..b]
+    
+    -- Ask for the operation to perform ('s' for sum, any other key for average)
+    putStrLn "Enter 's' for sum or any other key for average:"
+    operation <- getLine
+    
+    -- Call applicatorFunc with the range and operation
+    let result = applicatorFunc inpFunc (head operation) 
+    
+    -- Print the result
+    putStrLn ("Result = " ++ show(result))
